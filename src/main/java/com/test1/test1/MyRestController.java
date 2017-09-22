a2cf6229ea377b9db951527f7f40f1298669c832
lana12 <lana.senyor@gmail.com>
2017-09-22 14:16:46 +0300

    Add project

---
+++ src/main/java/com/test1/test1/MyRestController.java
@@ -0,0 +1,17 @@
+package com.test1.test1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class MyRestController {
+	
+	
+	@GetMapping
+	public String helloWorld() {
+		return "Hello World";
+	}
+
+}
